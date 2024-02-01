eddc29dd93f5e9dcfa10ce0214e66cb363b06c94
sanipop <67932273+sanipop@users.noreply.github.com>
2024-02-01 13:32:05 +0100

    Create 1-binary_tree_insert_left.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - place node at left
+ * 
+ * @parent: Addr to the ref node.
+ * @value: The value to store in the new node.
+ *
+ * Return: Null with empty par or err.
+ *         Otherwise - a pointer to the new node.
+ *
+ * Description: override left if on
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
+
+	return (new);
+}
