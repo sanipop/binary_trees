3a1d492f207ba68691097d254f6a35beecca8fce
sanipop <67932273+sanipop@users.noreply.github.com>
2024-02-01 14:56:08 +0100

    Create 124-sorted_array_to_avl.c

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,63 @@
+#include "binary_trees.h"
+
+/**
+ * create_tree - recuperate avl
+ *
+ * @node: the node addr
+ * @array: array of int
+ * @size: array size
+ * @mode: 1 to adding on the left, 2 to adding on the right
+ * Return: no return
+ */
+void create_tree(avl_t **node, int *array, size_t size, int mode)
+{
+	size_t middle;
+
+	if (size == 0)
+		return;
+
+	middle = (size / 2);
+	middle = (size % 2 == 0) ? middle - 1 : middle;
+
+	if (mode == 1)
+	{
+		(*node)->left = binary_tree_node(*node, array[middle]);
+		create_tree(&((*node)->left), array, middle, 1);
+		create_tree(&((*node)->left), array + middle + 1, (size - 1 - middle), 2);
+	}
+	else
+	{
+		(*node)->right = binary_tree_node(*node, array[middle]);
+		create_tree(&((*node)->right), array, middle, 1);
+		create_tree(&((*node)->right), array + middle + 1, (size - 1 - middle), 2);
+	}
+}
+
+/**
+ * sorted_array_to_avl - root node and create tree
+ *
+ * @array: the input in array
+ * @size: smthe size of the array
+ * Return: entry point of result 
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *root;
+	size_t middle;
+
+	root = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	middle = (size / 2);
+
+	middle = (size % 2 == 0) ? middle - 1 : middle;
+
+	root = binary_tree_node(root, array[middle]);
+
+	create_tree(&root, array, middle, 1);
+	create_tree(&root, array + middle + 1, (size - 1 - middle), 2);
+
+	return (root);
+}
